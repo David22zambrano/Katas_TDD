332bbfc8d900d82428c72e3603d8a272c7d0a122
David Sebastian <david.pena@sinco.co>
2025-10-31 15:57:41 -0500

    Test 3 Fase RED

--- Testing/tenisTest.cs
+++ Testing/tenisTest.cs
@@ -1,39 +1,47 @@
 namespace Testing;
- 
-  public class TennisScoreCalculatorTests
-  {
+
+public class TennisScoreCalculatorTests
+{
 // FAse RED
-      [Fact]
-      public void Score_Should_Return_LoveAll_When_0_0()
-      {
-          var calculator = new TennisScoreCalculator();
-          var result = calculator.Score(0, 0);
-          Assert.Equal("Love-All", result);
-      }
-      
-      [Fact]
-      public void Score_Should_Return_FifteenLove_When_1_0()
-      {
-          var calculator = new TennisScoreCalculator();
-          var result = calculator.Score(1, 0);
-          Assert.Equal("Fifteen-Love", result);
-      }
-  }
+    [Fact]
+    public void Score_Should_Return_LoveAll_When_0_0()
+    {
+        var calculator = new TennisScoreCalculator();
+        var result = calculator.Score(0, 0);
+        Assert.Equal("Love-All", result);
+    }
 
-  public class TennisScoreCalculator
-  {
-      public string Score(int player1Points, int player2Points)
-      {
-          if (player1Points == 0 && player2Points == 0)
-          {
-              return "Love-All";
-          }
+    [Fact]
+    public void Score_Should_Return_FifteenLove_When_1_0()
+    {
+        var calculator = new TennisScoreCalculator();
+        var result = calculator.Score(1, 0);
+        Assert.Equal("Fifteen-Love", result);
+    }
 
-          if (player1Points == 1 && player2Points == 0)
-          {
-              return "Fifteen-Love";
-          }
+    [Fact]
+    public void Score_Should_Return_LoveFifteen_When_0_1()
+    {
+        var calculator = new TennisScoreCalculator();
+        var result = calculator.Score(0, 1);
+        Assert.Equal("Love-Fifteen", result);
+    }
+}
 
-          return string.Empty;
-      }
-  }
+public class TennisScoreCalculator
+{
+    public string Score(int player1Points, int player2Points)
+    {
+        if (player1Points == 0 && player2Points == 0)
+        {
+            return "Love-All";
+        }
+
+        if (player1Points == 1 && player2Points == 0)
+        {
+            return "Fifteen-Love";
+        }
+
+        return string.Empty;
+    }
+}
